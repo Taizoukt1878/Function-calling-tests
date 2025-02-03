ead035686b44c5e81100239f2d361260e5ee81be
anouar taizoukt <ataizoukt@leyton.com>
2025-02-03 16:36:24 +0100

    first commit

---
+++ config.py
@@ -0,0 +1,33 @@
+# MARKET_DATA = [
+#     "address",  # The unique blockchain address of the token.
+#     "age",  # The duration since the token was first launched or created.
+#     "buy_count",  # The number of times the token has been bought.
+#     "change_1h",  # The percentage change in the token's price over the past hour.
+#     "change_24h",  # The percentage change in the token's price over the past 24 hours.
+#     # "holders",  # The number of unique addresses holding the token.
+#     # "id",  # A unique identifier for the token, typically used internally.
+#     # "img_url",  # URL of the token's logo or representative image.
+#     # "is_scam",  # A boolean indicating whether the token is identified as a scam.
+#     "last_transaction",  # The timestamp or details of the last transaction made with the token.
+#     "liquidity",  # The total liquidity available in the token's market.
+#     "market_cap",  # The total market capitalization of the token.
+#     # "name",  # The full name of the token.
+#     # "networkId",  # Identifier for the blockchain network on which the token exists.
+#     "price"  # The current price of the token.
+#     # "sell_count",  # The number of times the token has been sold.
+#     # "social_links",  # URLs or identifiers for the token's social media pages or community links.
+#     # "symbol",  # The abbreviated symbol or ticker of the token used on trading platforms.
+#     # "txn_count",  # The total number of transactions that have occurred involving this token.
+#     # "volume24",  # The total trading volume of the token in the past 24 hours.
+#     # "volume_change_24h"  # The percentage change in trading volume of the token over the past 24 hours.
+# ]
+
+MARKET_DATA = [
+    "price",  # The current price of the token.
+    "age",  # The duration since the token was first launched or created.
+    "buy_count",  # The number of times the token has been bought.
+    "change_1h",  # The percentage change in the token's price over the past hour.
+    "change_24h",  # The percentage change in the token's price over the past 24 hours.
+    "liquidity",  # The total liquidity available in the token's market.
+    "market_cap"  # The total market capitalization of the token.
+]
