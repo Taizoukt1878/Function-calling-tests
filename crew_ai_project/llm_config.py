2cfd5b189e9be75d798461f4ff9b8c47a6552cba
anouar taizoukt <ataizoukt@leyton.com>
2025-02-10 21:00:23 +0100

    llm-config

--- crew_ai_project/llm_config.py
+++ crew_ai_project/llm_config.py
@@ -2,12 +2,15 @@ import os
 from dotenv import load_dotenv
 from logger import setup_logger
 from crewai import LLM
+import streamlit as st
 
 logger = setup_logger()
 # Load environment variables from .env
 load_dotenv()
 
 
+# Load API key from .env file
+os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_KEY"]
 # load_dotenv(dotenv_path=".env")
 llm = LLM(model="nvidia_nim/meta/llama-3.1-70b-instruct", base_url="https://integrate.api.nvidia.com/v1", temperature=0)
 
