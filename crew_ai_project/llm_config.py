6dac64e937878ccc8b1046dbdf4ca29dd5d747fe
anouar taizoukt <ataizoukt@leyton.com>
2025-02-10 21:25:10 +0100

    llm config

--- crew_ai_project/llm_config.py
+++ crew_ai_project/llm_config.py
@@ -1,4 +1,4 @@
-import os
+# import os
 from dotenv import load_dotenv
 from logger import setup_logger
 from crewai import LLM
@@ -10,8 +10,8 @@ load_dotenv()
 
 
 # Load API key from .env file
-os.environ["OPENAI_API_KEY"] = str(st.secrets["OPENAI_KEY"])
+# os.environ["OPENAI_API_KEY"] = str(st.secrets["OPENAI_KEY"])
 # load_dotenv(dotenv_path=".env")
-llm = LLM(model="nvidia_nim/meta/llama-3.1-70b-instruct", base_url="https://integrate.api.nvidia.com/v1", temperature=0)
+llm = LLM(model="nvidia_nim/meta/llama-3.1-70b-instruct", base_url="https://integrate.api.nvidia.com/v1", temperature=0, api_key= st.secrets["OPENAI_KEY"])
 
 logger.info("OpenAI client initialized successfully.")
